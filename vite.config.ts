a972807ff80aa1ba4a00e90ec7200dd75b99b891
MSI\ASUS <riad_zakir@yahoo.com>
2025-02-22 01:13:40 +0800

    new build test

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,20 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: __dirname,
-  base: "/",
   cacheDir: './node_modules/.vite/scoreboard',
-
   server: {
     port: 4200,
     host: 'localhost',
   },
-
   preview: {
     port: 4300,
     host: 'localhost',
   },
-
   plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
   build: {
     outDir: './docs',
     emptyOutDir: true,
@@ -33,7 +23,6 @@ export default defineConfig({
       transformMixedEsModules: true,
     },
   },
-
   test: {
     watch: false,
     globals: true,
